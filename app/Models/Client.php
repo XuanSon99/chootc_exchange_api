27981c3d6b97d794d0d9f87b9c0995c6fe73e1f5
Your Name <you@example.com>
2023-07-27 10:59:46 +0700

    change

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -37,9 +37,9 @@ class Client extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-    
+
     public function getRouteKeyName()
     {
-        return 'name';
+        return 'phone';
     }
 }
