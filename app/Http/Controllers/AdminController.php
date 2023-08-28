ec1cd20583e59e92b3d68e00bf36fa18e33ddf80
Your Name <you@example.com>
2023-08-28 10:31:02 +0700

    change

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -133,13 +133,14 @@ class AdminController extends Controller
 
             $data = array_merge($data, $buy_list->get()->toArray());
             $data = array_merge($data, $sell_list->get()->toArray());
+            $data = collect($data)->sortByDesc('created_at')->values();
         }
 
         $perPage = 10;
         if ($request->has('perPage'))
             $perPage = $request->get('perPage');
 
-        $data = collect($data)->sortByDesc('created_at')->values();
+        
 
         return  $this->paginate($data, $perPage);
     }
