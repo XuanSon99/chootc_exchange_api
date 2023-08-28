c6ed04cc5e36aad0e765445c27fa302546b2083d
Your Name <you@example.com>
2023-08-28 10:21:13 +0700

    change

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -139,7 +139,7 @@ class AdminController extends Controller
         if ($request->has('perPage'))
             $perPage = $request->get('perPage');
 
-        $data = collect($data)->sortBy('id')->values();
+        $data = collect($data)->sortByDesc('id')->values();
 
         return  $this->paginate($data, $perPage);
     }
