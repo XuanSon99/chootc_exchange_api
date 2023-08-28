83a963a1d1837f3f4fe48c1ddf5965e246e7b249
Your Name <you@example.com>
2023-08-28 15:39:11 +0700

    change

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -49,9 +49,9 @@ class ClientController extends Controller
 
         $noti = new Notification([
             'phone' => $request->phone,
-            'color' => '#3ED050',
+            'color' => '#01c77d',
             'content' => 'Chúc mừng bạn đã tạo tài khoản thành công. Hãy KYC ngay để tăng hạn mức giao dịch.',
-            'url' => '/kyc/' . $request->phone,
+            'url' => '/profile',
             'is_readed' => 0
         ]);
         $noti->save();
